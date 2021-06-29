b70cf9da309b2f10b60548212573081c16f63147
jexnjeux <zieun.na@gmail.com>
2021-06-29 10:09:57 +0900

    변수에 타입작성하기

--- app.ts
+++ app.ts
@@ -1,10 +1,26 @@
-const container = document.getElementById('root');
-const ajax = new XMLHttpRequest();
+type Store = {
+  currentPage: number;
+  feeds: NewsFeed[];
+}
+
+type NewsFeed = {
+  id: number;
+  comments_count: number;
+  url: string;
+  user: string;
+  time_ago: string;
+  points: number;
+  title: string;
+  read?: boolean;
+}
+
+const container: HTMLElement | null = document.getElementById('root');
+const ajax: XMLHttpRequest = new XMLHttpRequest();
 const content = document.createElement('div');
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENTS_URL = 'https://api.hnpwa.com/v0/item/@id.json';
 
-const store = {
+const store: Store = {
   currentPage: 1,
   feeds: [],
 };
@@ -22,8 +38,15 @@ function makeFeeds(feeds) {
   return feeds;
 }
 
+function updateView (html) {
+  if (container !== null) {
+    container.innerHTML = html;
+  } else {
+    console.error('최상위 컨테이너가 없어 UI를 진행하지 못합니다.')
+  }
+}
 function newsFeed() {
-  let newsFeed = store.feeds;
+  let newsFeed: NewsFeed[] = store.feeds;
   let newsList = [];
   let template = `
   <div class='bg-gray-600 min-h-screen'>
@@ -84,7 +107,13 @@ function newsFeed() {
   );
   template = template.replace('{{__next_page__}}', store.currentPage + 1);
 
-  container.innerHTML = template;
+  updateView(template)
+
+  if (container !== null) {
+    container.innerHTML = template;
+  } else {
+    console.error('최상위 컨테이너가 없어 UI를 진행하지 못합니다.')
+  }
 }
 
 function newsDetail() {
@@ -147,10 +176,10 @@ function newsDetail() {
 
     return commentString.join('');
   }
-  container.innerHTML = template.replace(
+  updateView(template.replace(
     '{{__comments__}}',
     makeComment(newsContent.comments)
-  );
+    ))
 }
 
 function router() {
